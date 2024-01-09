cf7f8305e4085c454c84328a199816eef177fe87
Nico Frieß <nico.friess@eoda.de>
2024-01-09 15:42:54 +0100

    stuff

--- examples/shiny.R
+++ examples/shiny.R
@@ -1,31 +1,38 @@
 library(shiny)
+library(bslib)
 
-COLORS <- wesanderson::wes_palettes$BottleRocket1
+COLORS <- palette.colors(n = 9)
 
-nyc_boroughs <- sf::st_read("../data-raw/geo_export_d082ab44-4688-42c8-8fe8-2d5f5abe25d9.shp")
+nyc_boroughs <- sf::st_read("data-raw/geo_export_d082ab44-4688-42c8-8fe8-2d5f5abe25d9.shp")
 
-view <- basicPage(
-  selectInput("boro", "boro", choices = nyc_boroughs$boro_name),
-  selectInput("color", "color", choices = COLORS),
-  checkboxInput("visible", "visible", value = TRUE),
-  maplibreglOutput("map")
-)
 
-backend <- function(input, output) {
+ui <- function(request){
+  bslib::page_sidebar(
+    sidebar = tagList(
+      selectInput("boro", "boro", choices = nyc_boroughs$boro_name),
+      selectInput("color", "color", choices = COLORS),
+      checkboxInput("visible", "visible", value = TRUE)
+    ),
+    bslib::card(
+      maplibreglOutput("map")
+    )
+
+  )
+}
+
+server <- function(input,output,session){
   output$map <- renderMaplibregl(
     as_maplibregl(nyc_boroughs)
   )
 
-  observeEvent(list(input$color, input$visible, input$boro), {
+  observe({
+
     print(input$color)
-    print(input$visible)
-    print(input$boro)
     maplibreglProxy("map") %>%
-      #set_paint_property("fill-layer", "fill-color", input$color) %>%
-      #set_layout_property("fill-layer", "visibility", ifelse(input$visible, "visible", "none")) %>%
+      set_paint_property("fill-layer", "fill-color", input$color) %>%
       set_filter("fill-layer", c("==", "boro_name", input$boro)) %>%
       updateMaplibregl()
-  })
+  }) %>% bindEvent(input$color, input$boro, input$visible)
 }
 
-if (interactive()) shinyApp(view, backend)
+if (interactive()) shinyApp(ui, server)
